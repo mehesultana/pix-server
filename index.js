ded4523c58368746dd4ce06eabadeb0ffe5db141
mehesultana <sultanamehe01@gmail.com>
2021-12-21 21:27:58 +0600

    done

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 const app = express();
+const cors = require('cors');
 
 const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b4chv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
